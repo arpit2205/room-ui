64c6ccab71adf0d38684aeb9f7fc35d67d914744
Arpit Gupta <arpit.8.a@gmail.com>
2021-04-29 16:26:15 +0530

    gsap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { CssBaseline, Container, Typography, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import heroImage from "../images/desktop-image-hero-1.jpg";
@@ -6,20 +6,26 @@ import heroImageMob from "../images/mobile-image-hero-1.jpg";
 import arrow from "../images/icon-arrow.svg";
 import logo from "../images/logo.svg";
 
+import { TimelineLite, Power2 } from "gsap";
+
 const useStyles = makeStyles((theme) => ({
   heroImage: {
     width: "100%",
+    // position: "absolute",
+    opacity: 1,
   },
   logo: {
     position: "absolute",
     padding: theme.spacing(6, 8),
     transform: "scale(1.5)",
+    opacity: 1,
   },
   container: {
     margin: 0,
     padding: 0,
     overflow: "hidden",
     background: "white",
+    visibility: "hidden",
   },
   textContainer: {
     padding: theme.spacing(16, 10, 0, 10),
@@ -52,38 +58,73 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
   const classes = useStyles();
+  let image = useRef(null);
+  let imgContainer = useRef(null);
+  let roomLogo = useRef(null);
+  let container = useRef(null);
+  let bannerText = useRef(null);
+  let tl = new TimelineLite();
+
+  useEffect(() => {
+    tl.to(container, 0, {
+      css: { visibility: "visible", ease: Power2.easeInOut },
+    })
+      .from(image, 1, {
+        opacity: 0,
+        ease: Power2.easeInOut,
+      })
+      .from(roomLogo, 1, { opacity: 0, y: -40, ease: Power2.easeInOut })
+      .from(bannerText, 1, { x: -20, opacity: 0, ease: Power2.easeInOut });
+  }, []);
+
   return (
     <>
       <CssBaseline />
       <Container maxWidth={false} className={classes.container}>
-        <Grid container justify="center">
+        <Grid ref={(el) => (container = el)} container justify="center">
           <Grid item xs={12} md={7}>
-            <img src={logo} className={classes.logo} />
-            {window.screen.width > 600 ? (
-              <img src={heroImage} className={classes.heroImage} />
-            ) : (
-              <img src={heroImageMob} className={classes.heroImage} />
-            )}
+            <div ref={(el) => (imgContainer = el)}>
+              <img
+                ref={(el) => (roomLogo = el)}
+                src={logo}
+                className={classes.logo}
+              />
+              {window.screen.width > 600 ? (
+                <img
+                  ref={(el) => (image = el)}
+                  src={heroImage}
+                  className={classes.heroImage}
+                />
+              ) : (
+                <img
+                  ref={(el) => (image = el)}
+                  src={heroImageMob}
+                  className={classes.heroImage}
+                />
+              )}
+            </div>
           </Grid>
           <Grid item xs={12} md={5} className={classes.textContainer}>
-            <Typography variant="h3" className={classes.title}>
-              Discover innovative ways to decorate
-            </Typography>
-            <Typography
-              variant="body2"
-              style={{ marginTop: "32px" }}
-              className={classes.subtitle}
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem.
-            </Typography>
-            <button className={classes.btn}>
-              {/* prettier-ignore */}
-              <pre className={classes.btnText}>S H O P   N O W<img src={arrow} style={{marginLeft: '32px'}}/></pre>
-            </button>
+            <div ref={(el) => (bannerText = el)}>
+              <Typography variant="h3" className={classes.title}>
+                Discover innovative ways to decorate
+              </Typography>
+              <Typography
+                variant="body2"
+                style={{ marginTop: "32px" }}
+                className={classes.subtitle}
+              >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+              </Typography>
+              <button className={classes.btn}>
+                {/* prettier-ignore */}
+                <pre className={classes.btnText}>S H O P   N O W<img src={arrow} style={{marginLeft: '32px'}}/></pre>
+              </button>
+            </div>
           </Grid>
         </Grid>
       </Container>
--- src/components/Secondary.js
+++ src/components/Secondary.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { CssBaseline, Container, Typography, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { TimelineLite, Power2 } from "gsap";
 
 import dark from "../images/image-about-dark.jpg";
 import light from "../images/image-about-light.jpg";
@@ -34,15 +35,48 @@ const useStyles = makeStyles((theme) => ({
 
 const Secondary = () => {
   const classes = useStyles();
+
+  let left = useRef(null);
+  let center = useRef(null);
+  let right = useRef(null);
+
+  let tl = new TimelineLite();
+
+  useEffect(() => {
+    tl.from(left, 1, {
+      opacity: 0,
+      x: -20,
+      ease: Power2.easeInOut,
+      delay: 3,
+    })
+      .from(center, 1, {
+        opacity: 0,
+        y: -20,
+        ease: Power2.easeInOut,
+        // delay: -1,
+      })
+      .from(right, 1, { opacity: 0, x: 20, ease: Power2.easeInOut });
+  }, []);
+
   return (
     <>
       <CssBaseline />
       <Container maxWidth={false} className={classes.container}>
         <Grid container justify="center">
           <Grid item xs={12} md={4}>
-            <img src={dark} className={classes.image} />
+            <img
+              ref={(el) => (left = el)}
+              src={dark}
+              className={classes.image}
+            />
           </Grid>
-          <Grid item xs={12} md={4} className={classes.textContainer}>
+          <Grid
+            ref={(el) => (center = el)}
+            item
+            xs={12}
+            md={4}
+            className={classes.textContainer}
+          >
             <Typography variant="h6" className={classes.title}>
               ABOUT OUR FURNITURE
             </Typography>
@@ -55,7 +89,11 @@ const Secondary = () => {
             </Typography>
           </Grid>
           <Grid item xs={12} md={4}>
-            <img src={light} className={classes.image} />
+            <img
+              ref={(el) => (right = el)}
+              src={light}
+              className={classes.image}
+            />
           </Grid>
         </Grid>
       </Container>
